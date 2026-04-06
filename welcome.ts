1f9d6d38510c96033a7b258d33fe510954fb70ac
Mohammad Ayas <mohammadayas@MacBook-Air-M4.local>
2026-04-06 23:11:42 +0600

    Solved no-1 problem

---
+++ welcome.ts
@@ -0,0 +1,10 @@
+// Objective: Write a TypeScript program that outputs a welcome message.
+
+const message : string = "Hello World, I will complete this course successfully and become a Next level Web Developer!"
+
+console.log(message)
+
+
+
+
+
